f491ce0b987e9353a3ccc9da41d2935a75357b9f
ferchuse <sistemas@glifo.mx>
2020-01-31 13:11:27 -0600

    get destinos

---
+++ app/get_destinos.php
@@ -0,0 +1,58 @@
+<?php 
+	session_start();
+	if(count($_SESSION) == 0){
+		die("<div class='alert alert-danger'>Tu Sesión ha caducado, recarga la página.</div>");
+	}
+	include('../conexi.php');
+	$link = Conectarse();
+	$filas = array();
+	$respuesta = array();
+	
+	
+	$consulta = "SELECT * FROM precios_boletos 
+	LEFT JOIN origenes USING(id_origenes) 
+	LEFT JOIN (
+	SELECT id_origenes AS id_destinos,
+	nombre_origenes AS nombre_destinos
+	FROM origenes) t_destinos
+	USING(id_destinos)
+	WHERE precios_boletos.id_administrador = {$_SESSION["id_administrador"]}
+	ORDER BY nombre_destinos
+	";
+  
+	
+	$result = mysqli_query($link,$consulta);
+	if($result){
+		
+		if( mysqli_num_rows($result) == 0){
+			die("<div class='alert alert-danger'>No hay registros</div>");
+			
+		}
+		
+		
+		while($fila = mysqli_fetch_assoc($result)){
+			
+			$resultados[] = $fila ;
+			
+			
+		}
+		
+		
+		foreach($resultados as $i=> $registro){
+			
+				$respuesta["destinos"][] = $registro["nombre_destinos"];
+				$respuesta["precios"][] = number_format($registro["precio"], 0);
+		}
+		
+		
+	}
+	
+	else {
+		echo "<pre>Error en ".$consulta.mysqli_Error($link)."</pre>";
+		
+	}
+	
+	echo json_encode($respuesta);
+	
+	
+?>
