bbd48bc41a299de5f2ec10b42f405f07d4656fe8
ferchuse <sistemas@glifo.mx>
2021-01-28 13:06:30 -0600

    Quita postura

--- paginas/recaudacion/js/abonar_unidades.js
+++ paginas/recaudacion/js/abonar_unidades.js
@@ -582,7 +582,7 @@ function imprimirTicket(event){
 		
 		if($("#silent_print").val() == "SI" ){
 			//Impresion directa para el Usuario de Luis Manuel que recauda varias empresas
-			if($("#sesion_id_usuarios").val() == "56" || $("#sesion_id_usuarios").val() == "57" || $("#sesion_id_usuarios").val() == "58" || $("#sesion_id_usuarios").val() == "59"){
+			if($("#sesion_id_usuarios").val() == "56" || $("#sesion_id_usuarios").val() == "58" || $("#sesion_id_usuarios").val() == "59"){
 				$.ajax({
 					url: "http://localhost/imprimir_zitlalli.php",
 					method: "POST",
